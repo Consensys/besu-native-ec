3c1c9045dde7f8819751ea250036420960bdab20
Daniel Lehrner <daniel@io.builders>
2021-05-02 21:51:36 +0200

    fixed formatting
    Signed-off-by: Daniel Lehrner <daniel@io.builders>

--- src/utils.c
+++ src/utils.c
@@ -29,23 +29,23 @@ void set_error_message(char *error_message, const char *message_prefix) {
 
 unsigned char *hex_to_bin(const char *hex_string) {
 
-  if(hex_string == NULL)
+  if (hex_string == NULL)
     return NULL;
 
   size_t slength = strlen(hex_string);
-  if((slength % 2) != 0) // must be even
+  if ((slength % 2) != 0) // must be even
     return NULL;
 
   size_t dlength = slength / 2;
 
-  unsigned char* data = malloc(dlength);
+  unsigned char *data = malloc(dlength);
   memset(data, 0, dlength);
 
   size_t index = 0;
   while (index < slength) {
     char c = hex_string[index];
     int value = 0;
-    if(c >= '0' && c <= '9')
+    if (c >= '0' && c <= '9')
       value = (c - '0');
     else if (c >= 'A' && c <= 'F')
       value = (10 + (c - 'A'));
@@ -56,7 +56,7 @@ unsigned char *hex_to_bin(const char *hex_string) {
       return NULL;
     }
 
-    data[(index/2)] += value << (((index + 1) % 2) * 4);
+    data[(index / 2)] += value << (((index + 1) % 2) * 4);
 
     index++;
   }
